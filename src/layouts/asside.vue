a9ef745e2ee8d69982a364bb869c38b5b4179912
marcos <marcosvalens@gmail.com>
2021-06-10 08:17:18 +0000

    rutas y asside (falta header y footer component)

--- src/layouts/asside.vue
+++ src/layouts/asside.vue
@@ -1,5 +1,46 @@
 <template>
   <q-layout view="hHh Lpr lff">
+    <q-header reveal elevated class="text-white row justify-center">
+      <q-parallax src="../assets/banner.jpg" :height="200" class="desktop-only">
+        <q-toolbar
+          class="col-1 desktop-hide"
+          style="position: fixed; left:2vw; top:1vh"
+        >
+          <q-btn round color="secondary" icon="menu" @click="left = !left" />
+          <q-toolbar-title class="row justify-center"> </q-toolbar-title>
+        </q-toolbar>
+        <q-img
+          src="../assets/aloha.png"
+          spinner-color="white"
+          alt="Logo Alohas"
+          style="max-width: 150px"
+        />
+      </q-parallax>
+      <q-parallax src="../assets/banner.jpg" :height="100" class="desktop-hide">
+        <q-toolbar
+          class="col-1 desktop-hide"
+          style="position: fixed; left:2vw; top:1vh"
+        >
+          <q-btn round color="secondary" icon="menu" @click="left = !left" />
+          <q-toolbar-title class="row justify-center"> </q-toolbar-title>
+        </q-toolbar>
+        <q-img
+          src="../assets/aloha.png"
+          spinner-color="white"
+          alt="Logo Alohas"
+          style="max-width: 75px"
+        />
+      </q-parallax>
+      <q-tabs align="center" class="desktop-only">
+        <q-route-tab to="/menu/cbd" label="CBD" />
+        <q-route-tab to="/menu/fertilizantes" label="Fertilizantes" />
+        <q-route-tab to="/menu/semillas" label="Semillas" />
+        <q-route-tab to="/menu/sustratos" label="Sustratos" />
+        <q-route-tab to="/menu/iluminacion" label="Iluminación" />
+        <q-route-tab to="/menu/ventilacion" label="Ventilación" />
+        <q-route-tab to="/menu/parafernalia" label="Parafernalia" />
+      </q-tabs>
+    </q-header>
     <q-drawer
       show-if-above
       v-model="left"
@@ -22,6 +63,80 @@
     <q-page-container>
       <router-view />
     </q-page-container>
+    <q-footer class="bg-primary text-white">
+      <div id="animated-footer" class="row col-12 bg-dark items-center ">
+        <p class="col-12 text-red-1">
+          <span class="text-warning text-weight-bolder"
+            >Pedido a domicilio GRATUITO (Pedidos superiores a 30 €)</span
+          >
+          · LLamanos al <a href="tel:+34638342442">638342442</a>
+        </p>
+      </div>
+      <q-avatar class="q-ma-md">
+        <img src="../assets/aloha.png" alt="Logo Aloha Growshop" />
+      </q-avatar>
+      <div class="row justify-center">
+        <div class="q-pa-md col-lg-3 col-xs-12" style="max-width: 350px">
+          <q-list bordered>
+            <q-item>
+              <q-item-section class="text-center">Información</q-item-section>
+            </q-item>
+            <q-item clickable v-ripple>
+              <q-item-section avatar>
+                <q-avatar text-color="white" icon="phone" />
+              </q-item-section>
+              <q-item-section
+                ><a href="tel:+34638342442">Llámanos</a></q-item-section
+              >
+            </q-item>
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar text-color="white" icon="alternate_email" />
+              </q-item-section>
+              <q-item-section
+                ><a href="mailto:alohagrowshop@hotmail.com"
+                  >Contáctanos</a
+                ></q-item-section
+              >
+            </q-item>
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar text-color="white" icon="lab la-instagram" />
+              </q-item-section>
+              <q-item-section>
+                <a href="https://www.instagram.com/aloha_growshop/"
+                  >Instagram</a
+                >
+              </q-item-section>
+            </q-item>
+            <q-item>
+              <q-item-section avatar>
+                <q-avatar text-color="white" icon="room" />
+              </q-item-section>
+              <q-item-section>
+                Dirección
+                <a
+                  href="https://www.google.com/maps/place/Aloha+growshop/@39.582489,2.6588836,16z/data=!4m5!3m4!1s0x129793abc6e86fcb:0x80820a78c0ed8410!8m2!3d39.5823815!4d2.6578322"
+                  target="_blank"
+                  >Carrer de Guillem Galmés , 55</a
+                >
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </div>
+        <div class="q-pa-md col-lg-3 col-xs-12" style="max-width: 350px">
+          <iframe
+            height="250"
+            id="gmap_canvas"
+            src="https://maps.google.com/maps?q=aloha%20growshop&t=&z=17&ie=UTF8&iwloc=&output=embed"
+            frameborder="0"
+            scrolling="no"
+            marginheight="0"
+            marginwidth="0"
+          ></iframe>
+        </div>
+      </div>
+    </q-footer>
   </q-layout>
 </template>
 
